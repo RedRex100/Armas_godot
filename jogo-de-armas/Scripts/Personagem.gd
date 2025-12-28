1a1b1e4abece5bafca2a4524d91f8fc4f7ab4471
RedRex100 <fernando.redrex@gmail.com>
2025-12-28 11:25:03 -0300

    commit 1

--- jogo-de-armas/Scripts/Personagem.gd
+++ jogo-de-armas/Scripts/Personagem.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
-
 var speed := 200  # velocidade do personagem
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
+	var global = to_global(input_vector)
+	var accelerate 
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
+	if global.y == 0:
+		accelerate = 0
+	
 	input_vector = input_vector.normalized()
 
 	velocity = input_vector * speed
