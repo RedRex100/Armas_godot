70773594f8b02c88fb2694af68cf040cce79605b
RedRex100 <fernando.redrex@gmail.com>
2025-12-28 15:26:07 -0300

    gravidade

--- jogo-de-armas/Scripts/Personagem.gd
+++ jogo-de-armas/Scripts/Personagem.gd
@@ -1,19 +1,21 @@
 extends CharacterBody2D
 
 var speed := 200  # velocidade do personagem
-var accelerate = 9.8 
-var d
-
+var h_speed
+var d = 0
+var permissao = false
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	var global = to_global(input_vector)
-	
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	if global.y == 0:
-		pass
-	if global.y == 0 && Input.get_action_strength("ui_up") == 1:
-		pass
-	input_vector = input_vector.normalized()
-
-	velocity = input_vector * speed
+	position.x += speed * delta * (Input.get_action_strength("ui_right")- Input.get_action_strength("ui_left"))
+	print(position)
+	if position.y == 0 && Input.get_action_strength("ui_up") == 1:
+		permissao = true
+	if permissao:
+		d += delta
+		h_speed = speed - d * 196
+		position.y += h_speed * delta * -1
+		if position.y >= 0:
+			position.y = 0  
+			d = 0
+			permissao = false
+			h_speed = 0
 	move_and_slide()
